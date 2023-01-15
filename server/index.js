ff05d87b80ec8d2dc0c35fbf0f0e5c539c112492
fsociety <musharaf8651>
2023-01-15 21:05:51 +0530

    setting routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT=3000;
 
+app.get('/hello-world',(req,res)=>{
+    res.send("hello world");
+})
+
 app.listen(PORT,"0.0.0.0" ,()=>{
     console.log(`Connected at PORT ${PORT}`)
 });
